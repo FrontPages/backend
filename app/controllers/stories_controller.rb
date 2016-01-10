156d2b88a6c61eaafb4977b6d8bc3b89b947d070
Jay Pinho <jaypinho@gmail.com>
2016-01-10 18:53:32 -0500

    Add trending response

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,19 @@
+class StoriesController < ApplicationController
+
+	before_filter :make_api_public, only: [:trending]
+
+	respond_to :json
+
+	def trending
+
+    trending_query = "select t1.url, t2.title, t1.cnt
+      from (select url, count(title) cnt from headlines where created_at >= NOW() - '1 day'::INTERVAL group by url) t1
+      join (select distinct on (url) url, title from headlines where created_at >= NOW() - '1 day'::INTERVAL order by url, created_at desc) t2
+      on t1.url = t2.url order by t1.cnt desc"
+
+		@stories = ActiveRecord::Base.connection.execute(trending_query)
+		render json: {stories: @stories}
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :snapshots, :only => [:index]
   resources :sites, :only => [:index]
+  get 'trending', to: 'stories#trending'
 end
