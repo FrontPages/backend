b1d18d97c3ee0efeaaac9c0242527268e3ea6a76
Jay Pinho <jaypinho@gmail.com>
2016-01-10 19:11:12 -0500

    Tweak trending formula

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,7 +7,7 @@ class StoriesController < ApplicationController
 	def trending
 
     trending_query = "select t1.url, t2.title, t1.cnt
-      from (select url, count(title) cnt from headlines where created_at >= NOW() - '1 day'::INTERVAL group by url) t1
+      from (select url, count(distinct title) cnt from headlines where created_at >= NOW() - '1 day'::INTERVAL group by url having count(distinct title) >= 5) t1
       join (select distinct on (url) url, title from headlines where created_at >= NOW() - '1 day'::INTERVAL order by url, created_at desc) t2
       on t1.url = t2.url order by t1.cnt desc"
 
