5d4200bd145f1c8e784c6af0a7e2e49c44dccf67
Jay Pinho <jaypinho@gmail.com>
2016-01-12 18:03:15 -0500

    Add Impact method

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -24,4 +24,24 @@ class StoriesController < ApplicationController
 
 	end
 
+  def impact
+
+    if params[:threshold].present? && params[:threshold].to_i >= 50
+      threshold = params[:threshold]
+    elsif params[:threshold].present?
+      threshold = "50"
+    else
+      threshold = "100"
+    end
+
+    impact_query = "select t1.url, t2.title, t1.cnt
+      from (select url, count(*) cnt from headlines where created_at >= NOW() - '5 days'::INTERVAL group by url having count(*) >= #{threshold}) t1
+      join (select distinct on (url) url, title from headlines where created_at >= NOW() - '5 days'::INTERVAL order by url, created_at desc) t2
+      on t1.url = t2.url order by t1.cnt desc"
+
+      @stories = ActiveRecord::Base.connection.execute(trending_query)
+  		render json: {stories: @stories}
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :snapshots, :only => [:index]
   resources :sites, :only => [:index]
   get 'trending', to: 'stories#trending'
+  get 'impact', to: 'stories#impact'
 end
