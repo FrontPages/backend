77a933bc7d845aa155033d5ac4190a72a203f283
Jesse Pinho <jesse@jessepinho.com>
2016-01-19 08:57:23 -0600

    Use a query param for snapshot_id

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,6 +1,6 @@
 class HeadlinesController < ApplicationController
   def index
-    headlines = Headline.where(snapshot_id: params[:id])
+    headlines = Headline.where(snapshot_id: params[:snapshot_id])
     render json: headlines, each_serializer: HeadlineSerializer
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "impact", to: "stories#impact"
   get "sites", to: "sites#index"
   get "snapshots", to: "snapshots#index"
-  get "snapshots/:id/headlines", to: "headlines#index"
+  get "headlines", to: "headlines#index"
   get "trending", to: "stories#trending"
 end
