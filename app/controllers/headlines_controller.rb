20115660af38c964ac710342aa6f8b5c64ee376c
Jesse Pinho <jesse@jessepinho.com>
2016-01-23 17:50:51 -0600

    Fix CORS for headlines#index

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,4 +1,7 @@
 class HeadlinesController < ApplicationController
+  before_filter :make_api_public, only: [:index]
+  respond_to :json
+
   def index
     headlines = Headline.where(snapshot_id: params[:snapshot_id])
     render json: headlines, each_serializer: HeadlineSerializer
