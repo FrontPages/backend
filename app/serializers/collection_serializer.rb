185523ead9804b4e1197027ad788204bff122be3
Jay Pinho <jaypinho@gmail.com>
2016-02-09 11:15:14 -0500

    Update Ruby and shortern the snapshots in collections

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,5 +1,5 @@
 class CollectionSerializer < ActiveModel::Serializer
   # embed :ids, include: false
   attributes :id, :title, :subtitle, :permalink, :snapshots
-  # has_many :snapshots
+  has_many :snapshots
 end
