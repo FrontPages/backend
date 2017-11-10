f946045928a04d3338addaf6d56dd716d301a3a3
Jay Pinho <jaypinho@gmail.com>
2017-11-10 18:56:39 -0500

    Got it to somewhat work once I stopped setting the browser size to huge; now trying to enlarge it again

--- lib/automator.rb
+++ lib/automator.rb
@@ -25,7 +25,7 @@ module Automator
     # Add some pixels on top of the calculated dimensions for good
     # measure to make the scroll bars disappear
     #
-    # driver.manage.window.resize_to(width+100, height+100)
+    driver.manage.window.resize_to(width, height)
 
     begin
       driver.execute_script(site.script) unless site.script.nil?
